2657df14ed999e47d616ad917567700a23af9860
Elias <moukal@icloud.com>
2026-04-28 09:05:38 +0100

    post regex + finally upload video + flush video properly

--- main.py
+++ main.py
@@ -10,11 +10,17 @@ os.makedirs("tmp", exist_ok=True)
 
 app = App(token=os.environ["SLACK_BOT_TOKEN"], signing_secret=os.environ["SLACK_SIGNING_SECRET"])
 instagram_reel = re.compile(r"https://(?:www\.)?instagram\.com/reel/[^\s<>]+")
+instagram_post = re.compile(r"https://(?:www\.)?instagram\.com/p/[^\s<>]+")
 
+@app.message(instagram_post)
+def handle_post(message, say, client):
+    url = re.search(instagram_post, message["text"]).group(0)
+    say(f"no")
 
 @app.message(instagram_reel)
 def handle_reel(message, say, client):
     url = re.search(instagram_reel, message["text"]).group(0)
+    channel = message["channel"]
     ts_timestamp = str(int(time.time())) # pmo
     output_path = f"tmp/reel_{ts_timestamp}.mp4"
     yt_dlp_settings = {
@@ -27,8 +33,10 @@ def handle_reel(message, say, client):
         ydl.download([url])
     print(f"url: {url}")
     print(f"vidya path: {output_path}")
-    say(f"url: {url}")
-    os.remove(output_path)
+    try:
+        client.files_upload_v2(channel=channel, file=output_path, filename="reel.mp4")
+    finally:
+        os.remove(output_path)
 
 @app.event("message")
 def msg(body, logger):
