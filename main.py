fc36d6bf1cdd58933d124c2f31b6da52bb955a54
Elias <moukal@icloud.com>
2026-04-28 06:23:08 +0100

    bot works + wrestled with git

--- main.py
+++ main.py
@@ -2,16 +2,15 @@ import os
 from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
-
 load_dotenv()
 
 app = App(token=os.environ["SLACK_BOT_TOKEN"], signing_secret=os.environ["SLACK_SIGNING_SECRET"])
 
-@app.command("/ping")
-def handle_ping(ack, respond):
-    ack()
-    respond("pong")
-
+@app.message("ping")
+def ping(message, say):
+    print(f"Got message: {message}")
+    say("pong")
+    
 if __name__ == "__main__":
     handler = SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"])
     handler.start()
