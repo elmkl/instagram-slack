4088e154a5b891a5f1f239723e85c3b1e409eaf4
Elias <moukal@icloud.com>
2026-04-28 06:53:20 +0100

    start working on downloading logic

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import os
+import re
 from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 load_dotenv()
 
 app = App(token=os.environ["SLACK_BOT_TOKEN"], signing_secret=os.environ["SLACK_SIGNING_SECRET"])
+instagram_reel = re.compile(r"https://www\.instagram\.com/reel/[^\s]+")
+
+@app.message(instagram_reel)
+def handle_reel(message, say, client):
+    url = re.search(instagram_reel, message["text"]).group(0)
+    print(f"url: {url}")
+    say(f"url: {url}")
 
 @app.event("message")
 def msg(body, logger):
